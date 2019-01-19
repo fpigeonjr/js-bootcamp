2d6d707ea9db499debe3a335655ad642df8cce3c
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-01-18 20:32:31 -0600

    adds numbers

---
+++ basics/numbers.js
@@ -0,0 +1,15 @@
+let age = 40;
+let dogYears = (age + 1) / 7;
+
+console.log(dogYears);
+
+// challenge
+
+let studentScore = 41;
+let maxScore = 100;
+let percent = (studentScore / maxScore) * 100;
+
+console.log(
+  `The ${age} year old student scored a ${studentScore} out of ${maxScore}.`
+);
+console.log(`The students average is ${percent} percent`);

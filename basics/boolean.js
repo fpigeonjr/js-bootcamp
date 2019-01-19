7e130df1a1a6f33c72a39f33cabbaeab3e0dc087
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-01-18 21:02:57 -0600

    adds booleans

---
+++ basics/boolean.js
@@ -0,0 +1,16 @@
+let temp = 31;
+let isFreezing = temp < 32;
+
+console.log(isFreezing);
+
+// challenge
+const child = 7;
+const senior = 65;
+let age = 8;
+
+isChild = age <= child;
+isSenior = age >= senior;
+
+console.log(`you are ${age}`);
+console.log(`child: ${isChild}`);
+console.log(`senior: ${isSenior}`);

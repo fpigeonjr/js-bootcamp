b47c30cb047f37cd0358ab2c052b1b1b94e9c126
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-01-18 20:16:50 -0600

    adds strings

---
+++ basics/strings.js
@@ -0,0 +1,13 @@
+let firstName = "Frank";
+let lastName = "Pigeon";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// challenge
+
+let city = "san antonio";
+let country = "united states";
+let location = city + ", " + country;
+
+console.log(location);

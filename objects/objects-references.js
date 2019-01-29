d48306758c958324896bb3c23edfb291b427759a
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-01-28 21:50:05 -0600

    adds challenge functions

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -8,5 +8,29 @@ let addExpense = (account, amount) => {
   account.expenses += amount;
 };
 
-addExpense(myAccount, 2.5);
-console.log(myAccount);
+// addIncome - account, income
+let addIncome = (account, amount) => {
+  account.income += amount;
+};
+
+// resetAccount - reset acct to zero
+let resetAccount = account => {
+  account.expenses = 0;
+  account.income = 0;
+  return `account reset`;
+};
+
+// getAccountSummary
+let getAccountSummary = account => {
+  return `account for ${account.name} has $${account.income -
+    account.expenses}. $${account.income} in income and $${
+    account.expenses
+  } expenses.`;
+};
+
+// account for Andrew has $900. $1000 income and $100 expenses
+addIncome(myAccount, 1000);
+addExpense(myAccount, 100);
+console.log(getAccountSummary(myAccount));
+console.log(resetAccount(myAccount));
+console.log(getAccountSummary(myAccount));
